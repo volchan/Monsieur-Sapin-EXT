8e9817e72fdf3f0f3053c151eb786fbb66225e16
stephane delatorre <s.dlt@hotmail.fr>
2017-07-14 00:09:23 +0200

    done with website tab

---
+++ js/twitch_vods.js
@@ -0,0 +1,21 @@
+function getTwitchVods() {
+  var channel = "monsieursapin";
+  var clientId = "You won't get it here ;)";
+  var url = "https://api.twitch.tv/kraken/channels/" + channel + "/videos?broadcasts=true&limit=20&client_id=" + clientId;
+  var xhr_object = new XMLHttpRequest();
+  xhr_object.onreadystatechange = function() {
+    if (xhr_object.readyState==4 && xhr_object.status==200){
+      var data = JSON.parse(xhr_object.responseText);
+      var vods = data.videos
+      for (var i = 0; i < vods.length; i++) {
+        $('#twitch').append('<div class="twitch-card"><div class="thumbnail" style="background-image: url(' + vods[i].preview.replace(/320x240/, "320x180") + ')"></div><div class="infos">' + vods[i].title + '</div><a href="' + vods[i].url + '" class="vod-link"></a></div>');
+      };
+    }
+  };
+  xhr_object.open('GET', url, true);
+  xhr_object.send();
+};
+
+$(document).ready(function() {
+  getTwitchVods();
+});
