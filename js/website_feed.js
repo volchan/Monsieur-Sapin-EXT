6f9dc096292f64952650af4d739716ca0c64760e
stephane delatorre <s.dlt@hotmail.fr>
2017-07-20 11:11:33 +0200

    fixed rss display again

--- js/website_feed.js
+++ js/website_feed.js
@@ -2,10 +2,9 @@ function getFeed() {
   if ($('.website-card').length == 0) {
     $.get("http://monsieursapin.fr/feed", function (xml) {
       $(xml).find('item').each(function() {
-        var post = $(this);
-        $('#website').append('<div class="website-card"><div class="post-infos"><div class="post-title">' + post.find("title").text() + '</div><div class="post-description">' + post.find("description").text().replace(/]]>/, '') + '</div></div><a href="' + post.find("link").text() + '" class="post-link"></a></div>');
+        var post = $(this)
+        $('#website').append('<div class="website-card"><div class="post-infos"><div class="post-title">' + post.find("title").text() + '</div><div class="post-description">' + post.find("description").text().replace(/L\Warticle.*\s*]]>/, '') + '</div></div><a href="' + post.find("link").text() + '" class="post-link"></a></div>');
       });
-      $('.post-description > p:last-child').remove();
     });
   }
 };
